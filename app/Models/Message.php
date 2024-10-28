2b4957247584f64c03f8c3629757d109a07d78de
Martarp83 <martaruizperez@alumno.ieselrincon.es>
2024-10-28 13:44:33 +0000

    examen

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    use HasFactory;
+
+    // Permite la asignación masiva de estos campos
+    protected $fillable = ['id', 'text', 'subrayado'];
 }
